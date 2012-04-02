f9c926ed9efe3516fc1ea4d0f3910abd523949ba
Jinwoo <agreee45@hotmail.com>
2012-04-01 17:14:09 -0700

    Added test for deployer class.

---
+++ TestDeployer.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+
+# Created by Jinwoo Baek on 4/01/12.
+# Copyright (c) 2012 Jinwoo Baek. All rights reserved.
+
+require 'test/unit/assertions'
+require 'Deployer'
+
+class TestDeployer
+	include Test::Unit::Assertions
+
+end
+
+# ------------------------------------------------------------------
+
+deployer_tester = TestDeployer.new
+
