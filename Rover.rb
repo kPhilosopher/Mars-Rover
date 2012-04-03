601d68b672b7e439099a6e8399a2ce62140559c1
Jinwoo <agreee45@hotmail.com>
2012-04-02 19:00:27 -0700

    Added deploy and execution methods.

--- Rover.rb
+++ Rover.rb
@@ -5,19 +5,73 @@
 
 require 'CardinalDirections'
 require 'Coordinate'
+require 'Plateau'
+require 'GridStates'
 
 class Rover
 	attr_reader :start_coordinate
 	attr_reader :start_direction
-	# attr_accessor :current_direction
-	# attr_accessor :current_coordinate
-	# attr_accessor :plateau
+	attr_reader :current_direction
+	attr_reader :current_coordinate
+	attr_reader :plateau
+	attr_reader :instruction
 
 	def initialize(given_coordinate, given_direction)
 		@start_coordinate = given_coordinate
 		@start_direction = given_direction
 	end
 
+	def deploy(plateau)
+		@plateau = plateau
+		if plateau.state_at_coordinate(@start_coordinate) == GridStates::DefinedStates[1]
+			if plateau.set_state_at_coordinate(GridStates::DefinedStates[0], @start_coordinate)
+				@current_coordinate = @start_coordinate
+				@current_direction = @start_direction
+				return true
+			end
+		end
+		return false
+	end
 
+	def execute_instruction(instruction)
+		@instruction = instruction
+		@instruction.each_char { |chr|
+			if chr[/M/]
+				target_coordinate = target_coordinate_with_direction
+				if @plateau.state_at_coordinate(target_coordinate) == GridStates::DefinedStates[1]
+					@plateau.set_state_at_coordinate(GridStates::DefinedStates[0], target_coordinate)
+					previous_coordinate = @current_coordinate
+					@current_coordinate = target_coordinate
+					@plateau.set_state_at_coordinate(GridStates::DefinedStates[1], previous_coordinate)
+				else
+					# TODO: deal with situation where you can't go.
+					# puts "There is something in the way of the rover with starting coordinates of "
+					# "The square root of 5 is #{Math.sqrt(5)}"
+				end
+			elsif chr[/[LR]/]
+				if chr[/L/]
+					@current_direction = CardinalDirections.direction_when_rotated_left_with_direction(@current_direction)
+				elsif chr[/R/]
+					@current_direction = CardinalDirections.direction_when_rotated_right_with_direction(@current_direction)
+				end
+			end
+		}
+	end
 
+	def target_coordinate_with_direction
+		x_value = @current_coordinate.x
+		y_value = @current_coordinate.y
+
+		if @current_direction == 'N'
+			y_value += 1 
+		elsif @current_direction == 'S'
+			y_value -= 1
+		elsif @current_direction == 'W'
+			x_value -= 1
+		elsif @current_direction == 'E'
+			x_value += 1
+		end
+		return Coordinate.new(x_value, y_value)
+	end
 end
+
--- TestRover.rb
+++ TestRover.rb
@@ -4,8 +4,9 @@
 # Copyright (c) 2012 Jinwoo Baek. All rights reserved.
 
 require 'test/unit/assertions'
+require 'GridStates'
 require 'Rover'
-require 'Plateau'
+require 'GridPlateau'
 
 class TestRover
 	include Test::Unit::Assertions
@@ -15,36 +16,190 @@ class TestRover
 		assert_equal Rover, rover.class
 	end
 
-	def test_current_coordinate
+	def test_start_coordinate
 		rover = Rover.new(nil, nil)
 		assert_equal nil, rover.start_coordinate
 		assert_equal nil, rover.start_direction
 
 		# ----------------------------------------------------------
 
-		rover = Rover.new(Coordinate.new(10,2), nil)
-		assert_equal Coordinate.new(10,2).x, rover.start_coordinate.x
-		assert_equal Coordinate.new(10,2).y, rover.start_coordinate.y
+		rover = Rover.new(Coordinate.new(10, 2), nil)
+		assert_equal Coordinate.new(10, 2).x, rover.start_coordinate.x
+		assert_equal Coordinate.new(10, 2).y, rover.start_coordinate.y
 		assert_equal nil, rover.start_direction
 	end
 
-	def test_facing_direction
+	def test_start_direction
 		rover = Rover.new(nil, "S")
 		assert_equal "S", rover.start_direction
 	end
 
 	def test_deploy
 		coordinate = Coordinate.new(3, 2)
-		rover = Rover.new(coordinate, "N")
-		maximum_coordinate = Coordinate.new(5, 5)
-		plateau = Plateau.new(maximum_coordinate)
-		rover.deploy(plateau)
+		direction = "N"
+		rover = Rover.new(coordinate, direction)
+		maximum_coordinate = Coordinate.new(5, 6)
+		plateau = GridPlateau.new(maximum_coordinate)
 
+		assert_equal nil, rover.current_coordinate
+		assert_equal nil, rover.current_direction
+		assert_equal maximum_coordinate.x, plateau.maximum_coordinate.x
+		assert_equal maximum_coordinate.y, plateau.maximum_coordinate.y
+		assert_equal :unoccupied, plateau.state_at_coordinate(coordinate)
+
+		assert_equal true, rover.deploy(plateau)
+
+		assert_equal coordinate.x, rover.current_coordinate.x
+		assert_equal coordinate.y, rover.current_coordinate.y
+		assert_equal direction, rover.current_direction
+		assert_equal :occupied, rover.plateau.state_at_coordinate(coordinate)
 	end
+
+	def test_execute_instruction_M
+		coordinate = Coordinate.new(1, 2)
+		direction = "N"
+		rover = Rover.new(coordinate, direction)
+		maximum_coordinate = Coordinate.new(5, 5)
+		plateau = GridPlateau.new(maximum_coordinate)
+		assert_equal true, rover.deploy(plateau)
+
+		instruction = "M"
+		assert_equal true, rover.execute_instruction(instruction)
+		assert_equal instruction, rover.instruction
+		
+		expected_final_coordinate = Coordinate.new(1, 3)
+		expected_final_direction = "N"
+		assert_equal expected_final_coordinate.x, rover.current_coordinate.x
+		assert_equal expected_final_coordinate.y, rover.current_coordinate.y
+		assert_equal expected_final_direction, rover.current_direction
+		assert_equal GridStates::DefinedStates[1], rover.plateau.state_at_coordinate(coordinate)
+		assert_equal GridStates::DefinedStates[0], rover.plateau.state_at_coordinate(expected_final_coordinate)
+	end
+
+	def test_execute_instruction_L
+		coordinate = Coordinate.new(1, 2)
+		direction = "N"
+		rover = Rover.new(coordinate, direction)
+		maximum_coordinate = Coordinate.new(5, 5)
+		plateau = GridPlateau.new(maximum_coordinate)
+		assert_equal true, rover.deploy(plateau)
+
+		instruction = "L"
+		assert_equal true, rover.execute_instruction(instruction)
+		assert_equal instruction, rover.instruction
+		
+		expected_final_coordinate = Coordinate.new(1, 2)
+		expected_final_direction = "W"
+		assert_equal expected_final_coordinate.x, rover.current_coordinate.x
+		assert_equal expected_final_coordinate.y, rover.current_coordinate.y
+		assert_equal expected_final_direction, rover.current_direction
+	end
+
+	def test_execute_instruction_R
+		coordinate = Coordinate.new(1, 2)
+		direction = "N"
+		rover = Rover.new(coordinate, direction)
+		maximum_coordinate = Coordinate.new(5, 5)
+		plateau = GridPlateau.new(maximum_coordinate)
+		assert_equal true, rover.deploy(plateau)
+
+		instruction = "R"
+		assert_equal true, rover.execute_instruction(instruction)
+		assert_equal instruction, rover.instruction
+		
+		expected_final_coordinate = Coordinate.new(1, 2)
+		expected_final_direction = "E"
+		assert_equal expected_final_coordinate.x, rover.current_coordinate.x
+		assert_equal expected_final_coordinate.y, rover.current_coordinate.y
+		assert_equal expected_final_direction, rover.current_direction
+	end
+
+	def test_execute_instruction_set
+		text_file_name = "input_temp.txt"
+		content = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM"
+
+		coordinate = Coordinate.new(1, 2)
+		direction = "N"
+		rover = Rover.new(coordinate, direction)
+		maximum_coordinate = Coordinate.new(5, 5)
+		plateau = GridPlateau.new(maximum_coordinate)
+		assert_equal true, rover.deploy(plateau)
+
+		instruction = "LMLMLMLMM"
+		assert_equal true, rover.execute_instruction(instruction)
+		assert_equal instruction, rover.instruction
+		
+		expected_final_coordinate = Coordinate.new(1, 3)
+		expected_final_direction = "N"
+		assert_equal expected_final_coordinate.x, rover.current_coordinate.x
+		assert_equal expected_final_coordinate.y, rover.current_coordinate.y
+		assert_equal expected_final_direction, rover.current_direction
+	end
+
+	def test_target_coordinate_with_direction
+		coordinate = Coordinate.new(1, 2)
+		direction = "N"
+		rover = Rover.new(coordinate, direction)
+		maximum_coordinate = Coordinate.new(5, 5)
+		plateau = GridPlateau.new(maximum_coordinate)
+		assert_equal true, rover.deploy(plateau)
+
+		excpected_coordinate = Coordinate.new(1, 3)
+		assert_equal excpected_coordinate.x, rover.target_coordinate_with_direction.x
+		assert_equal excpected_coordinate.y, rover.target_coordinate_with_direction.y
+
+		# ------------------------------------------------------------------
+
+		coordinate = Coordinate.new(1, 2)
+		direction = "E"
+		rover = Rover.new(coordinate, direction)
+		maximum_coordinate = Coordinate.new(5, 5)
+		plateau = GridPlateau.new(maximum_coordinate)
+		assert_equal true, rover.deploy(plateau)
+
+		excpected_coordinate = Coordinate.new(2, 2)
+		assert_equal excpected_coordinate.x, rover.target_coordinate_with_direction.x
+		assert_equal excpected_coordinate.y, rover.target_coordinate_with_direction.y
+
+		# ------------------------------------------------------------------
+
+		coordinate = Coordinate.new(1, 2)
+		direction = "S"
+		rover = Rover.new(coordinate, direction)
+		maximum_coordinate = Coordinate.new(5, 5)
+		plateau = GridPlateau.new(maximum_coordinate)
+		assert_equal true, rover.deploy(plateau)
+
+		excpected_coordinate = Coordinate.new(1, 1)
+		assert_equal excpected_coordinate.x, rover.target_coordinate_with_direction.x
+		assert_equal excpected_coordinate.y, rover.target_coordinate_with_direction.y
+
+		# ------------------------------------------------------------------
+
+		coordinate = Coordinate.new(1, 2)
+		direction = "W"
+		rover = Rover.new(coordinate, direction)
+		maximum_coordinate = Coordinate.new(5, 5)
+		plateau = GridPlateau.new(maximum_coordinate)
+		assert_equal true, rover.deploy(plateau)
+
+		excpected_coordinate = Coordinate.new(0, 2)
+		assert_equal excpected_coordinate.x, rover.target_coordinate_with_direction.x
+		assert_equal excpected_coordinate.y, rover.target_coordinate_with_direction.y
+	end
+
 end
 
 # ------------------------------------------------------------------
+
 rover_tester = TestRover.new
 rover_tester.test_initialized_rover
-rover_tester.test_current_coordinate
-rover_tester.test_facing_direction
+rover_tester.test_start_coordinate
+rover_tester.test_start_direction
+rover_tester.test_deploy
+rover_tester.test_execute_instruction_M
+rover_tester.test_execute_instruction_L
+rover_tester.test_execute_instruction_R
+rover_tester.test_execute_instruction_set
+rover_tester.test_target_coordinate_with_direction
+
