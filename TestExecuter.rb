b7470c8e44759245cbf8fb3121810befd9fc16d6
Jinwoo <agreee45@hotmail.com>
2012-04-02 18:50:15 -0700

    Allow centralized testing.

---
+++ TestExecuter.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+# Created by Jinwoo Baek on 4/02/12.
+# Copyright (c) 2012 Jinwoo Baek. All rights reserved.
+
+require 'TestDeployer'
+require 'TestPlateau'
+require 'TestGridPlateau'
+require 'TestGrid'
+require 'TestRover'
+require 'TestParser'
+require 'TestCoordinate'
+require 'TestCardinalDirections'
