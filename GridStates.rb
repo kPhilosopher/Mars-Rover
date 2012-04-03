394eb9940d8721762a70c2a562b1ae3fbfdb8b47
Jinwoo <agreee45@hotmail.com>
2012-04-02 18:52:29 -0700

    Created a file to allow multiple files to share the constant values.

---
+++ GridStates.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+
+# Created by Jinwoo Baek on 4/01/12.
+# Copyright (c) 2012 Jinwoo Baek. All rights reserved.
+
+class GridStates
+	DefinedStates = [:occupied, :unoccupied, :off_the_plateau].freeze
+end
